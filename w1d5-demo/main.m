df2e866fcefb6811c8f3822242f5067a1fc39806
Roland Tecson <roland_tecson@hotmail.com>
2017-10-05 14:12:23 -0700

    Initial Commit

---
+++ w1d5-demo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  w1d5-demo
+//
+//  Created by Roland on 2017-10-05.
+//  Copyright © 2017 Roland. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
